c3e5c9c0bccb2e8fc460f2bf0f70d8710c49f457
Pavan K M <33823728+pavankm96@users.noreply.github.com>
2024-08-05 07:46:26 +0530

    Update LoginServlet.java

--- non_vul_java/src/main/java/com/example/LoginServlet.java
+++ non_vul_java/src/main/java/com/example/LoginServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.UUID;
+import org.apache.commons.text.StringEscapeUtils;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
